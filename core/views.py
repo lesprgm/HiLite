411e843e792478a8de24352c1061e94ef22ffa68
lesprgm <lesozzy5@gmail.com>
2025-05-24 20:53:48 -0400

    Updated views.py to handle PDF upload and highlight extraction and added a file upload form

--- core/views.py
+++ core/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .forms import PDFUploadForm
+from .utils import extract_highlights
+import tempfile
 
 # Create your views here.
+def upload_pdf(request):
+    highlights = None
+    if request.method == 'POST':
+        form = PDFUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as tmp:
+                for chunk in request.FILES['pdf_file'].chunks():
+                    tmp.write(chunk)
+                tmp_path = tmp.name
+            highlights = extract_highlights(tmp_path)
+            
+    else:
+        form = PDFUploadForm
+        
+    return render(request, 'upload.html', {'form': form, 'highlights': highlights})
+                    
