0bec5b8de1c890ec641e34d5a45f065cedd85a1e
lesprgm <lesozzy5@gmail.com>
2025-05-26 12:04:30 -0400

    Restore settings.py without secrets

--- hilite_project/settings.py
+++ hilite_project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#*36i3tgd_#1f+tkh3+)xl-_pl$8-=#2z8^cv5p+$@tzj=g!rr'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
