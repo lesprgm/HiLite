eac50105650343244dca8238ce92cc22c1535107
lesprgm <lesozzy5@gmail.com>
2025-05-27 23:41:22 -0400

    Configured PDF upload field with form-control w-100 Bootstrap classes and removed default label

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,11 @@
 from django import forms
 
 class PDFUploadForm(forms.Form):
-    pdf_file = forms.FileField()
+    pdf_file = forms.FileField(label = '')
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['pdf_file'].widget.attrs.update({
+            'class': 'form-control w-100'
+        })
